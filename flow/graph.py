77060dcd4d2a4e70be32b92b8e32aaf3a52fac9a
Andrew Houghton <houghtonandrew0@gmail.com>
2019-08-18 08:53:17 +1000

    Add edges

--- flow/graph.py
+++ flow/graph.py
@@ -1,6 +1,6 @@
 import networkx as nx
 
-class HeightGraph(nx.Graph):
+class HeightGraph(nx.DiGraph):
     def __init__(self, height_map):
         super().__init__()
         self._insert_height_map_edges(height_map)
@@ -10,3 +10,13 @@ class HeightGraph(nx.Graph):
             for x, item in enumerate(row):
                 self.add_node((y,x))
                 nx.set_node_attributes(self, {(y,x): {'height': item}})
+
+                if (y-1, x) in self:
+                    self.add_edge((y-1, x), (y,x))
+                    self.add_edge((y, x), (y-1,x))
+                if (y, x-1) in self:
+                    self.add_edge((y, x-1), (y,x))
+                    self.add_edge((y, x), (y,x-1))
+
+    def merge_equal_height_nodes(self):
+        pass
